8c67fc6a5353b94b1e507de65f5b40fd81d24442
nayacirdan <42298353+nayacirdan@users.noreply.github.com>
2020-07-10 15:53:52 +0300

    Updates

---
+++ precache-manifest.50fa95e5b1b425013c8d0a180d4ab407.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf8818fa43a76f7adb5cdbded235a419",
+    "url": "/star_wars/index.html"
+  },
+  {
+    "revision": "f5bead5cf0e04acfe914",
+    "url": "/star_wars/static/css/main.5639cfbd.chunk.css"
+  },
+  {
+    "revision": "cb37d69ca4094d003076",
+    "url": "/star_wars/static/js/2.39d4f0e9.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/star_wars/static/js/2.39d4f0e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5bead5cf0e04acfe914",
+    "url": "/star_wars/static/js/main.91c3b63e.chunk.js"
+  },
+  {
+    "revision": "9f2affc77291747fb8d3",
+    "url": "/star_wars/static/js/runtime-main.0a329fc0.js"
+  }
+]);
