7eaad96d5b7a22dd636707db4fa92ee77e4691a2
nayacirdan <42298353+nayacirdan@users.noreply.github.com>
2020-07-10 17:42:20 +0300

    ff

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import store from './store/store'
 ReactDOM.render(
   <React.StrictMode>
       <Provider store={store}>
-          <BrowserRouter>
+          <BrowserRouter basename={process.env.PUBLIC_URL}>
               <App />
           </BrowserRouter>
       </Provider>
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -2,3 +2,9 @@ export const makeIdFromUrl = (url) => {
     const urlArr = url.split('/');
     return urlArr[urlArr.length - 2];
 }
+
+export const changeUrls=(arr)=>{
+    return arr.map(str=>{
+        return str.substr(0,4)+'s'+str.substr(4);
+    });
+}
